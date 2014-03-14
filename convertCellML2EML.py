2a32c0d4d0a637f4b9279e716ed7b3f7959800a0
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2014-03-14 09:46:33 +0900

    convertCellML2EML.getProcess()

--- convertCellML2EML.py
+++ convertCellML2EML.py
@@ -35,6 +35,7 @@ __copyright__ = 'Keio University, RIKEN'
 __license__   = 'GPL'
 
 
+from ecell.eml import *
 from CellML import *
 
 from copy import deepcopy
@@ -47,12 +48,16 @@ class ecell3Model( object ):
         
         self.Entity_type_strings = [ 'System', 'Variable', 'Process', 'Stepper' ]
         
-        self.Systems   = {}
+        self.Systems   = []
         self.Variables = []
         self.Processes = []
         
         self._get_Systems( CellML )
-        #print '\nSystem:\n%s\n' % self.Systems
+        
+        print '\nSystems:\n'
+        for S in self.Systems:
+            print '%s:%s' % ( S.path, S.ID )
+        print '\n'
         
         self._get_Variables( CellML )
         self._get_Processes( CellML )
@@ -60,44 +65,51 @@ class ecell3Model( object ):
     ##-------------------------------------------------------------------------------------------------
     def _get_Systems( self, CellML ):
         
-        self.Systems [ '/' ] = ''  ## component (ID) : ecell3_path
+        self.Systems.append( System( '', '/' ) )
         
         for me, children in CellML.containment_hierarchies.iteritems():
             
-            self._get_sub_Systems( '/', me, children )
+            self._get_sub_Systems( self.Systems[ 0 ], me, children )
         
     ##-------------------------------------------------------------------------------------------------
-    def _get_sub_Systems( self, parent, me, children ):
+    def _get_sub_Systems( self, parent_System, me, children ):
         
-        if self.Systems[ parent ] == '':
-            self.Systems[ me ] = '/'
-            
-        else:
-            self.Systems[ me ] = self._connect_paths( ( self.Systems[ parent ], parent ) )
+        my_System = System( self._convert_Entity_to_path( parent_System ), me )
+        self.Systems.append( my_System )
         
         for child, grandchildren in children.iteritems():
-            self._get_sub_Systems( me, child, grandchildren )
+            self._get_sub_Systems( my_System, child, grandchildren )
         
     ##-------------------------------------------------------------------------------------------------
     def _get_Variables( self, CellML ):
         
         for actual, properties in CellML.grobal_variables.iteritems():
             
-            path, ID = actual
+            component, name = actual
             
             if properties[ 'variable' ].has_key( 'initial_value' ):
                 self.Variables.append( Variable( 
-                    self._connect_paths( ( self.Systems[ path ], path ) ),
-                    ID,
+                    self._get_path_of_super_component( component ),
+                    name,
                     properties[ 'variable' ][ 'initial_value' ] ) )
             
             else:
                 self.Variables.append( Variable( 
-                    self._connect_paths( ( self.Systems[ path ], path ) ),
-                    ID ) )
+                    self._get_path_of_super_component( component ),
+                    name ) )
             
-#            print 'Variable:%s:%s' % ( self.Variables[ -1 ].path, ID )
-    
+            print 'Variable:%s:%s' % ( self.Variables[ -1 ].path, self.Variables[ -1 ].ID )
+        
+        for S in self.Systems:
+            if len( [ V for V in self.Variables 
+                if ( V.ID == 'SIZE' and ( V.path == self._get_path_of_super_component( S.ID ) ) ) ] ) == 0:
+                
+                self.Variables.append( Variable( 
+                    self._get_path_of_super_component( S.ID ),
+                    'SIZE',
+                    1.0 ) )
+        
+        
     ##-------------------------------------------------------------------------------------------------
     def _get_Processes( self, CellML ):
         
@@ -105,7 +117,7 @@ class ecell3Model( object ):
             
             variables = [ self._get_grobal_variable( v, c_name, CellML ) for v in c_property[ 'variable' ] ]
             math      = [ m for m in c_property[ 'math' ] ]
-            path      = self._connect_paths( ( self.Systems[ c_name ], c_name ) )
+            path      = self._get_path_of_super_component( c_name )
             
 #            print '\nSystem: %s' % c_name
 #            print 'variables: %s' % variables
@@ -119,10 +131,10 @@ class ecell3Model( object ):
             
             for local_variable in pr[ 'connection' ]:
                 if ( local_variable[ 'component' ] == component ) and ( local_variable[ 'variable' ] == variable ):
-                    path, ID = grobal_variable
-                    return ( variable, self._connect_paths( ( self.Systems[ path ], path ) ), ID )
+                    _component, _ID = grobal_variable
+                    return ( variable, ':{0}:{1}'.format( self._get_path_of_super_component( _component ), _ID ) )
         
-        return ( variable, False, False )
+        raise TypeError, "_get_grobal_variable(): global variable is not found."
     
     ##-------------------------------------------------------------------------------------------------
     def _append_Process( self, math, variables, path ):
@@ -143,26 +155,31 @@ class ecell3Model( object ):
         Expression, VariableReferenceList = self._get_Expression_and_VariableReferenceList( math, variables )
         
         self.Processes.append( Process( cls, path, ID, VariableReferenceList, Expression ) )
-        print '\n%s:%s:%s' % ( self.Processes[ -1 ].cls, self.Processes[ -1 ].path, self.Processes[ -1 ].ID )
-        print '    %s' % self.Processes[ -1 ].Expression
-        print '    %s\n' % self.Processes[ -1 ].VariableReferenceList
+#        print '\n%s:%s:%s' % ( self.Processes[ -1 ].cls, self.Processes[ -1 ].path, self.Processes[ -1 ].ID )
+#        print '    %s' % self.Processes[ -1 ].Expression
+#        print '    %s\n' % self.Processes[ -1 ].VariableReferenceList
     
     ##-------------------------------------------------------------------------------------------------
     def _get_Expression_and_VariableReferenceList( self, math, variables ):
         
         math_Element = deepcopy( math.right_side )
         
-        VariableReferenceList = [ list( v ) for v in variables 
-            if ( v[ 0 ] in [ ci.text for ci in math_Element.findall( './/' + math.tag[ 'ci' ] ) ] ) ]
+        ci_list = [ ci for ci in math_Element.findall( './/' + math.tag[ 'ci' ] ) ]
+        ci_text_list = [ ci.text for ci in ci_list ]
+        
+        if not ( math.variable in ci_text_list ):
+            ci_text_list.append( math.variable )
+        
+        VariableReferenceList = [ list( v ) for v in variables if ( v[ 0 ] in ci_text_list ) ]
         
         for vr in VariableReferenceList:
             if math.variable == vr[ 0 ]:
-                vr.append( 1 )
+                vr.append( '1' )
             else:
-                vr.append( 0 )
+                vr.append( '0' )
         
         for v in variables:
-            for ci in math_Element.iterfind( './/' + math.tag[ 'ci' ] ):
+            for ci in ci_list:
                 if ci.text == v[ 0 ]:
                     ci.text = v[ 0 ] + '.Value'
         
@@ -191,24 +208,46 @@ class ecell3Model( object ):
         for dir in paths:
             if dir != '/':
                 _paths.extend( dir.split( '/' ) )
-        paths = _paths
         
-        if paths[ 0 ] in self.Entity_type_strings:
-            paths( 0 )
+        if _paths[ 0 ] in self.Entity_type_strings:
+            _paths( 0 )
         
-        paths = [ p for p in paths if p != '' ]
+        _paths = [ p for p in _paths if p != '' ]
         
-        if paths == []:
+        if _paths == []:
             return '/'
-        elif not ( paths[ 0 ] in ( '.', '..' ) ):
-            paths.insert( 0, '' )
+        elif not ( _paths[ 0 ] in ( '.', '..' ) ):
+            _paths.insert( 0, '' )
         
-        return '/'.join( paths )
+        return '/'.join( _paths )
+    
+    ##-------------------------------------------------------------------------------------------------
+    def _convert_Entity_to_path( self, Entity ):
+        
+        return self._connect_paths( ( Entity.path, Entity.ID ) )
+    
+    ##-------------------------------------------------------------------------------------------------
+    def _get_path_of_super_component( self, super_component_name ):
+        
+        for S in self.Systems:
+           if S.ID == super_component_name:
+               return self._convert_Entity_to_path( S )
+        return False
+
+
+class System( object ):
+        
+    def __init__( self, path, ID, Name = '__none__' ):
+        
+        self.path  = path
+        self.ID    = ID
+        self.Name  = Name
+        
 
 
 class Variable( object ):
         
-    def __init__( self, path, ID, Value = False, Name = '' ):
+    def __init__( self, path, ID, Value = 'NaN', Name = '__none__' ):
         
         self.path  = path
         self.ID    = ID
@@ -219,20 +258,57 @@ class Variable( object ):
 
 class Process( object ):
         
-    def __init__( self, cls, path, ID, VariableReferenceList, Expression = '0.0', Name = '' ):
+    def __init__( self, cls, path, ID, VariableReferenceList, Expression = '0.0', Name = '__none__' ):
         
         self.cls        = cls
         self.path       = path
         self.ID         = ID
         self.Expression = Expression
         self.Name       = Name
-        self.VariableReferenceList = []
+        self.VariableReferenceList = VariableReferenceList
 
+
+def get_eml( cellml_file_path ):
+    
+    model = ecell3Model( CellML( cellml_file_path ) )
+    
+    eml = Eml()
+    
+    ## Stepper
+    
+    eml.createStepper( 'FixedODE1Stepper'   , 'ODE' )
+    eml.createStepper( 'DiscreteTimeStepper', 'DT' )
+    
+    default_Stepper_ID = 'ODE'
+    
+    for s in model.Systems:
+        
+        _FullID = 'System:{0}:{1}'.format( s.path, s.ID )
+        eml.createEntity( 'System', _FullID )
+        eml.setEntityProperty( _FullID, 'Name',  [ s.Name ] )
+        eml.setEntityProperty( _FullID, 'StepperID', [ default_Stepper_ID ] )
+        
+    for v in model.Variables:
+        
+        _FullID = 'Variable:{0}:{1}'.format( v.path, v.ID )
+        eml.createEntity( 'Variable', _FullID )
+        eml.setEntityProperty( _FullID, 'Value', [ str( v.Value ) ] )
+        eml.setEntityProperty( _FullID, 'Name',  [ v.Name ] )
+
+    for p in model.Processes:
+        
+        _FullID = 'Process:{0}:{1}'.format( p.path, p.ID )
+        eml.createEntity( p.cls, _FullID )
+        eml.setEntityProperty( _FullID, 'Name', [ p.Name ] )
+        eml.setEntityProperty( _FullID, 'Expression', [ p.Expression ] )
+        eml.setEntityProperty( _FullID, 'VariableReferenceList', p.VariableReferenceList )
+
+    return eml
 
 
 ########  MAIN  ########
 
-CM = CellML( './tentusscher_noble_noble_panfilov_2004_a.cellml' )
+#CM = CellML( './tentusscher_noble_noble_panfilov_2004_a.cellml' )
 
 #print CM.root_node.tag
 #print CM.root_node.attrib
@@ -243,13 +319,17 @@ CM = CellML( './tentusscher_noble_noble_panfilov_2004_a.cellml' )
 #print '\n\ncontainment_hierarchies:\n%s' % CM.containment_hierarchies
 #print '\n\nconnections:\n%s' % CM.connections
 
-for component in CM.components.itervalues():
-    for math in component[ 'math' ]:
+#for component in CM.components.itervalues():
+#    for math in component[ 'math' ]:
 #       print '\n' + math.get_expression_str()
-       pass
+#       pass
 
-model = ecell3Model( CM )
+#model = ecell3Model( CM )
 
-for component, FullID in model.Systems.iteritems():
-    #print '{} : {}'.format( component, FullID )
-    pass
+#for component, FullID in model.Systems.iteritems():
+#    #print '{} : {}'.format( component, FullID )
+#    pass
+
+eml = get_eml( './tentusscher_noble_noble_panfilov_2004_a.cellml' )
+eml.save( './tentusscher_noble_noble_panfilov_2004_a.eml' )
+
